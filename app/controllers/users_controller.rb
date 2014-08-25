9bfed9175b4f795942bb21594fcf41c14a822992
brentbautista1 <bbenedictbautista@gmail.com>
2014-08-26 05:33:29 +0800

    Add user mic microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
-  		render 'new'
+  		render index
   	end
   end
 
@@ -52,13 +53,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
